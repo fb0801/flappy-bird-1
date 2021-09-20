47d047a3648e0d87a2adc5b4a43fdc1a5519741c
Farhan <74499472+fb0801@users.noreply.github.com>
2021-09-20 17:09:04 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -3,11 +3,12 @@ var block = document.getElementById('block');
 var hole = document.getElementById('hole');
 var character = document.getElementById('character');
 var jumping = 0;
-
+var score = 0; //for the score
 
 hole.addEventListener('animationiteration', () =>{
     var random = -((Math.random() * 300) + 150);
     hole.style.top = random + "px";
+    score++;
 });
 
 setInterval(function(){
@@ -16,6 +17,15 @@ setInterval(function(){
     if(jumping ==0){
         character.style.top = (characterTop + 3) + 'px';
 
+    }
+    var blockLeft = 
+    parseInt(window.getComputedStyle(block).getPropertyValue('top'));
+
+    //detection for if the bird hits the pipe
+    if(characterTop){
+        alert('GAME OVER! score: '+score);
+        character.style.top = 100 + 'px';
+        score= 0;
     }
 },10);
 
