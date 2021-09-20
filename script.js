2f34e8878f9f35e1530ad338f324662ae4f5cd29
Farhan <74499472+fb0801@users.noreply.github.com>
2021-09-20 16:50:53 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
+//variables for the game
 var block = document.getElementById('block');
 var hole = document.getElementById('hole');
+var character = document.getElementById('character');
+
 
 hole.addEventListener('animationiteration', () =>{
     var random = -((Math.random() * 300) + 150);
     hole.style.top = random + "px";
 });
+
+setInterval(function(){
+    var characterTop = 
+    parseInt(window.getComputedStyle(character).getPropertyValue('top'));
+    characterTop.style.top = (characterTop + 3) + 'px';
+},10);
+
+function jump(){
+    
+}
