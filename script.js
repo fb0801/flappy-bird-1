0e0795314f38ca1d4733e09768679b0b86b8521b
Farhan <74499472+fb0801@users.noreply.github.com>
2021-09-20 16:54:45 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 var block = document.getElementById('block');
 var hole = document.getElementById('hole');
 var character = document.getElementById('character');
+var jumping = 0;
 
 
 hole.addEventListener('animationiteration', () =>{
@@ -12,9 +13,15 @@ hole.addEventListener('animationiteration', () =>{
 setInterval(function(){
     var characterTop = 
     parseInt(window.getComputedStyle(character).getPropertyValue('top'));
-    characterTop.style.top = (characterTop + 3) + 'px';
+    if(jumping ==0){
+        character.style.top = (characterTop + 3) + 'px';
+
+    }
 },10);
 
 function jump(){
-    
+    jumping = 1;
+    var jumpInterval = setInterval(function(){
+
+    },10 );
 }
